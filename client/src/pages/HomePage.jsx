f052a0e4367657510f1f925056addf373504b4cc
Stevo99 <Sfailla1983@gmail.com>
2020-02-24 17:24:02 -0500

    added Homepage section to app

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import TextComponent from '../components/TextComponent';
+import Button from '../components/Button';
+
+// import TextComponent from '../components/TextComponent';
+{
+	/* <TextComponent text="This is a todo app that displays an express backend api and authentication using web tokens" />
+			<TextComponent text="to get started just register and then you will have access to the todo app" />
+			<TextComponent text="if you already registered than just click on the button below to login and go straight to the app" /> */
+}
 
 const HomePage = () => (
 	<div className="App-Layout homepage">
-		<div className="homepage--left-box">
+		<div className="homepage__layout">
 			<h1 className="homepage__title">What TODO</h1>
-			<TextComponent text="This is a todo app that displays an express backend api and authentication using web tokens" />
-			<TextComponent text="to get started just register and then you will have access to the todo app" />
-			<TextComponent text="if you already registered than just click on the button below to login and go straight to the app" />
+			<div className="homepage__button-wrapper">
+				<div className="homepage__register-button">
+					<Link to="/register">
+						<Button name="REGISTER" className="form-button" />
+					</Link>
+				</div>
+				<div className="homepage__register-button">
+					<Link to="/login">
+						<Button name="LOGIN" className="form-button" />
+					</Link>
+				</div>
+			</div>
 			<div className="homepage__footer-message">
 				<p>Steven Failla &copy; 2018</p>
 			</div>
