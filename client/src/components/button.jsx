c1d62aa9b025d2f1de123fd6ce1e0901ab85a453
Stevo99 <Sfailla1983@gmail.com>
2020-02-25 18:20:32 -0500

    added rest op for props

--- client/src/components/button.jsx
+++ client/src/components/button.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Button = ({ name, className }) => {
-	return <button className={className}>{name}</button>;
+const Button = ({ name, ...otherProps }) => {
+	return <button {...otherProps}>{name}</button>;
 };
 
 export default Button;
