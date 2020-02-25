bb9a1d77c4a425c2f53c1941c914a3c3ece4f6eb
Stevo99 <Sfailla1983@gmail.com>
2020-02-25 14:27:06 -0500

    changed classNames for better readability

--- client/src/components/InputComponent.jsx
+++ client/src/components/InputComponent.jsx
@@ -1,30 +1,45 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-
-const InputComponent = ({ className, placeholder, handleOnChange, label, name, type, value }) => {
-    return (
-        <label className="Form-Type" style={{marginBottom: '.5rem', fontSize: '2.3rem', fontWeight: '600'}}>
-            {label}
-        <input 
-            className={className}
-            type={type} 
-            name={name}
-            value={value} 
-            onChange={handleOnChange}
-            placeholder={placeholder} />
-        </label>
-    )
-}
+const InputComponent = ({
+	className,
+	placeholder,
+	handleOnChange,
+	label,
+	name,
+	type,
+	value
+}) => {
+	return (
+		<label
+			className="text-gradient"
+			style={{
+				marginBottom: '.5rem',
+				fontSize: '2.3rem',
+				fontWeight: '600'
+			}}
+		>
+			{label}
+			<input
+				className={className}
+				type={type}
+				name={name}
+				value={value}
+				onChange={handleOnChange}
+				placeholder={placeholder}
+			/>
+		</label>
+	);
+};
 
 InputComponent.propTypes = {
-    handleOnChange: PropTypes.func.isRequired,
-    className: PropTypes.string,
-    placeholder: PropTypes.string.isRequired,
-    label: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    type: PropTypes.string.isRequired,
-    value: PropTypes.string
-}
+	handleOnChange: PropTypes.func.isRequired,
+	className: PropTypes.string,
+	placeholder: PropTypes.string.isRequired,
+	label: PropTypes.string.isRequired,
+	name: PropTypes.string.isRequired,
+	type: PropTypes.string.isRequired,
+	value: PropTypes.string
+};
 
-export default InputComponent
+export default InputComponent;
