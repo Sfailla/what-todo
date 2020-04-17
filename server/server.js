22c92368055f688080f670e5658a3ec35277c2dd
Stevo99 <Sfailla1983@gmail.com>
2020-04-17 10:12:14 -0400

    redirected http to https in production

--- server/server.js
+++ server/server.js
@@ -14,12 +14,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/todos', todos);
 app.use('/users', users);
 
-app.use(express.static(path.resolve(__dirname, '..', 'client', 'build')));
+app.use(
+	express.static(path.resolve(__dirname, '..', 'client', 'build'))
+);
+
+if (process.env.NODE_ENV === 'production') {
+	//Serving the files on the CLIENT folder
+	app.use((req, res, next) => {
+		// if not an https server, the site will be redirected to https
+		if (req.header('x-forwarded-proto') !== 'https') {
+			res.redirect(`https://${req.header('host')}${req.url}`);
+		} else {
+			next();
+		}
+	});
+}
 
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, '..', 'client', 'public', 'index.html'), err => {
-		if (err) res.status(500).send(err);
-	});
+	res.sendFile(
+		path.resolve(__dirname, '..', 'client', 'public', 'index.html'),
+		err => {
+			if (err) res.status(500).send(err);
+		}
+	);
 });
 
 app.listen(process.env.PORT, () =>
